a3ea1429e854282672f20a58a220012c843b8f2f
Dillon91 <holden.dillon@gmail.com>
2017-12-05 18:56:55 -0600

    Update and rename Header.h to LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,14 +1,11 @@
 #pragma once
 #include <stdio.h>
 #include <iostream>
-
 using namespace std;
 template <typename Type>
 class LinkedList
-
 {
 public:
-
 	LinkedList();
 	~LinkedList();
 	int getLength();
@@ -17,145 +14,105 @@ public:
 	void remove(Type);
 	int length;
 	struct Node
-
 	{
-
 		Type val;
-
 		Node* next;
-
 	};
-
 	Node* head, *tail;
-
 };
-
-
 /**
 * Sets head and tail to null, sets length to be empty.
 */
-
 template <typename Type>
 LinkedList<Type>::LinkedList()
-
 {
-	head = tail = NULL;
+	head = nullptr;
+	tail = nullptr;
 	length = 0;
 }
-
 /**
 * Destructor for the Linked List.
 */
-
 template <typename Type>
 LinkedList<Type>::~LinkedList()
-
 {
-	Node* prev = NULL;
+	Node* prev = nullptr;
 	Node* it = head;
-	while (it != NULL)
-
+	while (it != nullptr)
 	{
 		prev = it;
 		it = it->next;
 		delete prev;
-
 	}
-
 }
-
 /**
 * Prints the whole linked list.
 */
-
 template <typename Type>
 void LinkedList<Type>::print()
-
 {
 	int index = 0;
 	Node* it = head;
-	while (it != NULL)
-
+	while (it != nullptr)
 	{
-		//if (it == NULL) return;
 		std::cout << "{" << index << ":" << it->val << "}";
-		if (it->next != NULL)
-
+		if (it->next != nullptr)
 		{
 			std::cout << "->";
 		}
-
 		it = it->next;
 		index++;
 	}
 }
-
 /**
 * @return the number of nodes in the linked list.
 */
-
 template <typename Type>
 int LinkedList<Type>::getLength()
-
 {
 	return length;
 }
-
 template <typename Type>
 void LinkedList<Type>::add(Type value)
-
 {
 	Node* newNode = new Node;
 	newNode->val = value;
-	newNode->next = NULL;
-
-	if (head == NULL)
+	newNode->next = nullptr;
+	if (head == nullptr)
 	{
 		head = tail = newNode;
 	}
-
 	else
-
 	{
 		tail->next = newNode;
 		tail = newNode;
 	}
-
 	length++;
-
 }
-
 /**
 * Removes all nodes that have the value passed in.
 * @param value The value you want to remove.
 */
-
 template <typename Type>
 void LinkedList<Type>::remove(Type value)
-
 {
 	Node* it = head;
-	Node* prev = NULL;
-	while (it->next != NULL)
-
+	Node* prev = nullptr;
+	while (it->next != nullptr)
 	{
 		if (it->val == value)
 		{
-			if (prev == NULL)
+			if (prev == nullptr)
 			{
 				head = it->next;
 				delete it;
 			}
-
 			else
-
 			{
 				prev->next = it->next;
 				delete it;
 			}
-
 		}
-
 		prev = it;
 		it = it->next;
 	}
