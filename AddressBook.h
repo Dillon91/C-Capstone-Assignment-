68d5bfb384f8fd82c7294f79a164643982809866
Dillon91 <holden.dillon@gmail.com>
2017-12-07 11:56:15 -0600

    Update AddressBook.h

--- AddressBook.h
+++ AddressBook.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <iostream>
 #include "LinkedList.h"
-#include "Address.h"
+
+
+
+class Address
+{
+public:
+	// Create an Address using these 4 params
+	Address(std::string name = "Not Entered",
+		std::string streetAddress = "Not Entered",
+		std::string anniversaryDate = "XX-XX-XXXX",
+		std::string birthdayDate = "XX-XX-XXXX");
+	~Address();
+	// Let other files print out addresses
+	friend std::ostream& operator<<(std::ostream& os, const Address& curAddress);
+	std::string name;
+	std::string streetAddress;
+	std::string anniversaryDate;
+	std::string birthdayDate;
+
+};
+
+
 class AddressBook : public LinkedList<Address>
 {
 public:
